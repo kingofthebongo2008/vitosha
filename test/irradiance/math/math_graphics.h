c7890a57c498cbd6920d3c12139e4bd070f9e2bf
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2012-07-22 21:15:37 +0300

    cosmetic changes

--- test/irradiance/math/math_graphics.h
+++ test/irradiance/math/math_graphics.h
@@ -77,16 +77,10 @@ namespace math
 	{
 		float4x4 m;
 
-		static const uint32_t __declspec( align(16) ) mask_x[4] = { 0xFFFFFFFF, 0x0, 0, 0};
-		static const uint32_t __declspec( align(16) ) mask_y[4] = { 0, 0xFFFFFFFF, 0, 0};
-		static const uint32_t __declspec( align(16) ) mask_z[4] = { 0, 0, 0xFFFFFFFF, 0};
-
-		static const float4 identity_r3  = {0.0f, 0.0f, 0.0f, 1.0f};
-
-		m.r[0] = and( v, reinterpret_cast< const float4*> (&mask_x)[0] );
-		m.r[1] = and( v, reinterpret_cast< const float4*> (&mask_y)[0] );
-		m.r[2] = and( v, reinterpret_cast< const float4*> (&mask_z)[0] );
-		m.r[3] = identity_r3;
+		m.r[0] = and( v, mask_x() );
+		m.r[1] = and( v, mask_y() );
+		m.r[2] = and( v, mask_z() );
+		m.r[3] = identity_r3();
 
 		return m;
 	}
@@ -152,8 +146,6 @@ namespace math
 		float4x4 m;
 
 		static const float4 negate_y = {1.0f, -1.0f,1.0f,1.0f};
-		static const float4 identity_r0  = {1.0f, 0.0f, 0.0f, 0.0f};
-		static const float4 identity_r3  = {0.0f, 0.0f, 0.0f, 1.0f};
 
 		float4 v1 = _mm_set_ss(cos_angle);
 		float4 v2 = _mm_set_ss(sin_angle);
@@ -163,10 +155,10 @@ namespace math
 		float4 v5 = negate_y;
 		float4 v6 = mul(v4, v5);
 
-		m.r[0] = identity_r0;
+		m.r[0] = identity_r0();
 		m.r[1] = v3;
 		m.r[2] = v6;
-		m.r[3] = identity_r3;
+		m.r[3] = identity_r3();
 
 		return m;
 	}
@@ -176,8 +168,6 @@ namespace math
 		float4x4 m;
 
 		static const float4 negate_z = {1.0f, 1.0f,-1.0f,1.0f};
-		static const float4 identity_r1  = {0.0f, 1.0f, 0.0f, 0.0f};
-		static const float4 identity_r3  = {0.0f, 0.0f, 0.0f, 1.0f};
 
 		float4 v1 = _mm_set_ss(cos_angle);
 		float4 v2 = _mm_set_ss(sin_angle);
@@ -188,9 +178,9 @@ namespace math
 		float4 v6 = mul(v4, v5);
 
 		m.r[0] = v6;
-		m.r[1] = identity_r1;
+		m.r[1] = identity_r1();
         m.r[2] = v3;
-		m.r[3] = identity_r3;
+		m.r[3] = identity_r3();
 
 		return m;
 	}
@@ -200,9 +190,6 @@ namespace math
 		float4x4 m;
 
 		static const float4 negate_x = {-1.0f, 1.0f,1.0f,1.0f};
-		static const float4 identity_r2  = {0.0f, 0.0f, 1.0f, 0.0f};
-		static const float4 identity_r3  = {0.0f, 0.0f, 0.0f, 1.0f};
-
 		float4 v1 = _mm_set_ss(cos_angle);
 		float4 v2 = _mm_set_ss(sin_angle);
 
@@ -213,8 +200,8 @@ namespace math
 
 		m.r[0] = v3;
 		m.r[1] = v6;
-		m.r[2] = identity_r2;
-		m.r[3] = identity_r3;
+		m.r[2] = identity_r2();
+		m.r[3] = identity_r3();
 
 		return m;
 	}
@@ -240,44 +227,77 @@ namespace math
 		return rotation_z(sin_angle, cos_angle);
 	}
 
-    //creates left handed view matrix
+	//creates left handed view matrix
 	inline float4x4 view(float4 eye_position, float4 eye_direction, float4 up_direction)
 	{
-		//eye_direction and up_direction should be normalized;
-		//eye_direction and up_direction w components should be zero
+			//eye_direction and up_direction should be normalized;
+			//eye_direction and up_direction w components should be zero
 
-		static const uint32_t   __declspec( align(16) ) mask_w[4] = { 0, 0, 0, 0xFFFFFFFF};
-		static const uint32_t	__declspec( align(16) ) mask_xyz[4] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0};
-		static const float4	identity_r3 = {0.0f, 0.0f, 0.0f, 1.0f};
+			float4x4 m1;
 
-		float4x4 m1;
-		float4x4 m2;
+			float4 v0 = cross3(up_direction, eye_direction);
+			v0 = normalize3(v0);
 
-		float4 v0 = cross3(up_direction, eye_direction);
-		v0 = normalize3(v0);
+			float4 v1 = cross3(eye_direction, v0);
+			float4 v2 = eye_direction;
 
-		float4 v1 = cross3(eye_direction, v0);
-		float4 v2 = eye_direction;
+			//form the translation
+			float4 negative_eye_position = negate(eye_position);
 
-		//form the translation
-		float4 negative_eye_position = negate(eye_position);
+			float4 d0 = dot3(v0, negative_eye_position);
+			float4 d1 = dot3(v1, negative_eye_position);
+			float4 d2 = dot3(v2, negative_eye_position);
 
-		float4 d0 = dot3(v0, negative_eye_position);
-		float4 d1 = dot3(v1, negative_eye_position);
-		float4 d2 = dot3(v2, negative_eye_position);
+			float4  mask = mask_w();
+			m1.r[0] = select( v0, d0, mask );
+			m1.r[1] = select( v1, d1, mask ); 
+			m1.r[2] = select( v2, d2, mask ); 
+			m1.r[3] = identity_r3();
 
-        float4  mask = load4(mask_w);
-        m1.r[0] = select( v0, d0, mask );
-		m1.r[1] = select( v1, d1, mask ); 
-		m1.r[2] = select( v2, d2, mask ); 
-		m1.r[3] = identity_r3;
-
-
-		m2 = transpose(m1);
-		return m2;
+			return transpose(m1);
 	}
 
-    //creates left handed view matrix
+	//creates left handed inverse view matrix
+	inline float4x4 inverse_view(float4 eye_position, float4 eye_direction, float4 up_direction)
+	{
+			//eye_direction and up_direction should be normalized;
+			//eye_direction and up_direction w components should be zero
+
+			static const uint32_t   __declspec( align(16) ) mask_yw[4] = { 0, 0xFFFFFFFF, 0, 0xFFFFFFFF};
+			static const uint32_t	__declspec( align(16) ) mask_xyz[4] = { 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF, 0};
+
+			float4x4 m1;
+			float4x4 m2;
+
+			float4 v0 = cross3(up_direction, eye_direction);
+			v0 = normalize3(v0);
+
+			float4 v1 = cross3(eye_direction, v0);
+			float4 v2 = eye_direction;
+
+			float4 position = eye_position;
+			float4 d0 = dot3(v0, position);
+			float4 d1 = dot3(v1, position);
+			float4 d2 = dot3(v2, position);
+
+			float4  mask = load4(mask_yw);
+			m1.r[0] = v0;
+			m1.r[1] = v1;
+			m1.r[2] = v2;
+			m1.r[3] = identity_r3();
+
+			float4 d0_d2 = shuffle<x,x,x,x>(d0, d2);
+			float4 d1_d3 = shuffle<x,x,w,w>(d1, identity_r3());
+
+			m2.r[0] = identity_r0();
+			m2.r[1] = identity_r1();
+			m2.r[2] = identity_r2();
+			m2.r[3] = select( d0_d2, d1_d3, mask);
+
+			return mul(m2, m1);
+	}
+
+	//creates left handed view matrix
 	inline float4x4 look_at_lh(float4 eye_position, float4 look_at_position, float4 up_direction)
 	{
 		float4 v1 = sub(look_at_position, eye_position);
@@ -286,11 +306,18 @@ namespace math
 		return view(eye_position, v2, v3);
 	}
 
-    //creates left handed perspective projection matrix
-	inline float4x4 perspective_lh(float view_width, float view_height, float z_near, float z_far)
+	//creates left handed view matrix
+	inline float4x4 inverse_look_at_lh(float4 eye_position, float4 look_at_position, float4 up_direction)
 	{
-		static const uint32_t	__declspec( align(16) )	mask_x[4] = { 0xFFFFFFFF, 0, 0, 0};
-		static const uint32_t	__declspec( align(16) )	mask_y[4] = { 0, 0xFFFFFFFF, 0, 0 };
+		float4 v1 = sub(look_at_position, eye_position);
+		float4 v2 = normalize3(v1);
+		float4 v3 = normalize2(up_direction);
+		return inverse_view(eye_position, v2, v3);
+	}
+
+	//creates left handed perspective projection matrix
+	inline float4x4 inverse_perspective_lh(float view_width, float view_height, float z_near, float z_far)
+	{
 		static const uint32_t	__declspec( align(16) )	mask_yzw[4] = { 0, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 		static const float4	identity_r3 = {0.0f, 0.0f, 0.0f, 1.0f};
 
@@ -304,12 +331,43 @@ namespace math
 		float4x4 m;
 
 		float4 v2 = shuffle<x,y,x,y>(v1, ze);
-		m.r[0] = and(v2, reinterpret_cast< const float4*> (&mask_x)[0] );
+		m.r[0] = and(v2, mask_x() );
 
 		float4 v3 = and(v1, reinterpret_cast< const float4*> (&mask_yzw)[0] );
 
 		v2 = swizzle<x,y,x,x>(v1);
-		m.r[1] = and ( v2, reinterpret_cast< const float4*> (&mask_y)[0] );
+		m.r[1] = and ( v2, mask_y());
+
+		float4 v4 = shuffle<z,w,z,w>(v3, identity_r3);
+
+		m.r[2] = swizzle<z,z,x,w>(v4);
+		m.r[3] = swizzle<z,z,y,z>(v4);
+
+		return m;
+	}
+
+    //creates left handed perspective projection matrix
+	inline float4x4 perspective_lh(float view_width, float view_height, float z_near, float z_far)
+	{
+		static const uint32_t	__declspec( align(16) )	mask_yzw[4] = { 0, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
+		static const float4	identity_r3 = {0.0f, 0.0f, 0.0f, 1.0f};
+
+		float a = 2 * z_near;
+
+		float r = z_far / (z_far - z_near);
+
+		float4 v1 = set( a / view_width, a / view_height, r, -r * z_near );
+		float4 ze = zero();
+
+		float4x4 m;
+
+		float4 v2 = shuffle<x,y,x,y>(v1, ze);
+		m.r[0] = and(v2, mask_x());
+
+		float4 v3 = and(v1, reinterpret_cast< const float4*> (&mask_yzw)[0] );
+
+		v2 = swizzle<x,y,x,x>(v1);
+		m.r[1] = and ( v2, mask_y());
 
 		float4 v4 = shuffle<z,w,z,w>(v3, identity_r3);
 
@@ -322,8 +380,6 @@ namespace math
     //creates left handed perspective projection matrix
 	inline float4x4 perspective_fov_lh(float fov, float aspect_ratio, float z_near, float z_far)
 	{
-		static const uint32_t	__declspec( align(16) )	mask_x[4] = { 0xFFFFFFFF, 0, 0, 0};
-		static const uint32_t	__declspec( align(16) )	mask_y[4] = { 0, 0xFFFFFFFF, 0, 0 };
 		static const uint32_t	__declspec( align(16) )	mask_yzw[4] = { 0, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 		static const float4	identity_r3 = {0.0f, 0.0f, 0.0f, 1.0f};
 
@@ -339,10 +395,10 @@ namespace math
 		float4x4 m;
 
 		float4 v2 = shuffle<x,y,x,y>(v1, ze);
-		m.r[0] = and(v2, reinterpret_cast< const float4*> (&mask_x)[0] );
+		m.r[0] = and(v2, mask_x());
 
 		v2 = swizzle<x,y,x,x>(v1);
-		m.r[1] = and ( v2, reinterpret_cast< const float4*> (&mask_y)[0] );
+		m.r[1] = and ( v2, mask_y());
 
 		float4 v3 = and(v1, reinterpret_cast< const float4*> (&mask_yzw)[0] );
 		float4 v4 = shuffle<z,w,z,w>(v3, identity_r3);
@@ -356,7 +412,6 @@ namespace math
     //creates left handed orthographic projection matrix
 	inline float4x4 orthographic_lh(float view_width, float view_height, float z_near, float z_far)
 	{
-		static const uint32_t	__declspec( align(16) )	mask_x[4] = { 0xFFFFFFFF, 0, 0, 0};
 		static const uint32_t	__declspec( align(16) )	mask_yzw[4] = { 0, 0xFFFFFFFF, 0xFFFFFFFF, 0xFFFFFFFF };
 		static const float4	identity_r3 = {0.0f, 0.0f, 0.0f, 1.0f};
 
@@ -370,7 +425,7 @@ namespace math
 		float4x4 m;
 
 		float4 v2 = shuffle<x,y,x,y>(v1, ze);
-		m.r[0] = and(v2, reinterpret_cast< const float4*> (&mask_x)[0] );
+		m.r[0] = and(v2, mask_x());
 
 		float4 v3 = and(v1, reinterpret_cast< const float4*> (&mask_yzw)[0] );
 
@@ -502,7 +557,6 @@ namespace math
 		return	v3;
 	}
 
-
     //returns near and far from a projection matrix
     inline std::tuple<float, float> extract_near_far( math::float4x4 p )
     {
@@ -593,20 +647,18 @@ namespace math
         //  h = 1 / ( 1 + e)
 
 
-        float4 v = cross3( s, t );
-        float4 e = quaternion_dot(s, t);
-        float4 h = splat ( 1.0f / (1.0f + get_x(e) ) ) ;
+        const float4 v = cross3( s, t );
+        const float4 e = quaternion_dot(s, t);
+        const float4 h = splat ( 1.0f / (1.0f + get_x(e) ) ) ;
 
         float4 vx = mul ( h, mul ( v, splat_x(v) ) ); // hvxvx, hvxvy, hvxvz
         float4 vy = mul ( h, mul ( v, splat_y(v) ) ); // hvxvy, hvyvy, hvyvz
         float4 vz = mul ( h, mul ( v, splat_z(v) ) ); // hvxvz, hvyvz, hvzvz
 
-		static const float4 identity_r3                             = { 0.0f, 0.0f, 0.0f, 1.0f};
-        static const float4 masks                                   = { 1.0f, -1.0f, 0.0f, 0.0f};
-        static const uint32_t	__declspec( align(16) )	mask_w[4]   = { 0,      0, 0, 0xFFFFFFFF };
+        const float4 masks	= { 1.0f, -1.0f, 0.0f, 0.0f};
 
-        float4  v_mask_w = load4( reinterpret_cast<const float*> ( &mask_w[0] )  )  ;
-        float4  mask = masks;
+		const float4  v_mask_w = mask_w();
+        const float4  mask = masks;
 
         float4 v_p = select( v, e, v_mask_w);
         float4 v_px = swizzle<x,x,x,z>(mask);
@@ -628,7 +680,7 @@ namespace math
 		m.r[0] = add( vx, v_1);
 		m.r[1] = add( vy, v_2);
 		m.r[2] = add( vz, v_3);
-		m.r[3] = identity_r3;
+		m.r[3] = identity_r3();
 
         return m;
     }
@@ -653,26 +705,24 @@ namespace math
 
     inline float4 arc_ball_point_on_unit_sphere( float x_, float y_, float r, float4 c )
     {
-        static const uint32_t	__declspec( align(16) )	mask_z[4]   = { 0, 0, 0xFFFFFFFF, 0 };
-        static const uint32_t	__declspec( align(16) )	mask_w[4]   = { 0, 0, 0, 0xFFFFFFFF };
 
-        float4 s0       = point3( x_, y_, 0.0f );
-        float4 v0       = div ( sub ( s0, c ), splat(r) ); // v0 = (s0-c) / r
-        float4 dot      = dot2( v0, v0 );
+        const float4 s0       = point3( x_, y_, 0.0f );
+        const float4 v0       = div ( sub ( s0, c ), splat(r) ); // v0 = (s0-c) / r
+        const float4 dot      = dot2( v0, v0 );
 
-        float4 v0_1     = mul ( v0, rsqrt( dot ) );     //normalize v0
-        float4 z_       = sqrt ( sub ( one() , dot ) );
+        const float4 v0_1     = mul ( v0, rsqrt( dot ) );			//normalize v0
+        const float4 z_       = sqrt ( sub ( one() , dot ) );
         
-        float4 v_mask_z = load4( mask_z );
-        float4 v0_2     = select ( v0, z_, v_mask_z ) ; // v0.x, v0.y,  sqrt( 1 - (vx * vy)^2), 0
+        const float4 v_mask_z = mask_z();
+        const float4 v0_2     = select ( v0, z_, v_mask_z ) ;		// v0.x, v0.y,  sqrt( 1 - (vx * vy)^2), 0
 
-        float4 cmp      = swizzle<x,x,x,x>(dot);
-        float4 mask     = compare_gt ( cmp , one() );   // dot > 1.0f  
+        const float4 cmp      = swizzle<x,x,x,x>(dot);
+        const float4 mask     = compare_gt ( cmp , one() );		// dot > 1.0f  
 
-        float4 result   = select ( v0_2, v0_1, mask ); // select of two points, depending on the coordinates. snap on the ball
+        float4 result   = select ( v0_2, v0_1, mask );		// select of two points, depending on the coordinates. snap on the ball
 
         // make point
-        float4 v_mask_w = load4(mask_w); 
+        const float4 v_mask_w = mask_w();
         result = select ( result, one(), v_mask_w) ;
         return result;
     }
@@ -680,19 +730,17 @@ namespace math
     //create a rotation quaternion from v_0 to v_1 on the 3 sphere
     inline float4 arc_ball_quaternion( float4 v_0, float4 v_1)
     {
-        static const uint32_t	__declspec( align(16) )	mask_w[4]   = { 0, 0, 0, 0xFFFFFFFF };
-
         float4 dot      = dot3 ( v_0, v_1 );
         float4 cross    = cross3(v_0, v_1 );
 
         // make point
-        float4 v_mask_w = load4(mask_w); 
+        float4 v_mask_w = mask_w();
         float4 result = select ( cross, dot, v_mask_w) ;
         return result;
     }
 
 
-    //given a point on the 3 sphere and a constraint axis, returns a point on the 3 sphere and plane perpendicular to the axis and through the center
+    //given a point on the 3 sphere and a constraint axis, returns a point on the 3 sphere that lies on a plane perpendicular to the axis and through the center
     inline float4 arc_ball_constraint_on_axis( float4 sphere_point, float4 axis )
     {
         const  float4 dot       = dot3( sphere_point, axis);
@@ -702,8 +750,6 @@ namespace math
         const float4 result_1   = {1.0f, 0.0f, 0.0f, 1.0f };    
         const float4 mask_res_2 = {-1.0f, 1.0f, 0.0f, 0.0f };
         const float4 mask_res_3 = { 0.0f, 0.0f, 0.0f, 1.0f };
-
-        static const uint32_t	__declspec( align(16) )	mask_w[4]   = { 0, 0, 0, 0xFFFFFFFF };
 
         float4 result_2 = swizzle< y, x, w, w> ( axis );        
         result_2 = mad( result_2, mask_res_2, mask_res_3);  // -axis.y, axis.x, 0, 0
@@ -731,7 +777,7 @@ namespace math
 
         
         // set w= 1.0f;
-        return select( result_123, one(), load4(mask_w) );
+        return select( result_123, one(), mask_w() );
     }
 
     inline float4 arc_ball_closest_axis( float4 sphere_point, const float4* axis, uint32_t axis_count)
