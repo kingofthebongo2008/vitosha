3833fca118778af655126d85fae0563223a24004
kingofthebongo2008 <stefan.dyulgerov@gmail.com>
2013-03-10 00:12:34 +0200

    Fix profile timer bugs. It was not compiling + added default values if windows functions start to fail

--- test/irradiance/sys/sys_profile_timer.h
+++ test/irradiance/sys/sys_profile_timer.h
@@ -20,19 +20,20 @@ namespace sys
         /// reset() makes the timer start over counting from 0.0 seconds.
         void reset()
         {
-            uint64_t pf;
+            uint64_t pf = 1;
             QueryPerformanceFrequency( (LARGE_INTEGER *)&pf );
             m_freq = 1.0 / (double)pf;
-            QueryPerformanceCounter( (LARGE_INTEGER *)&baseTime_ );
+            m_base_time = 0;
+            QueryPerformanceCounter( (LARGE_INTEGER *)&m_base_time );
         }
         
         /// seconds() returns the number of seconds (to very high resolution)
         /// elapsed since the timer was last created or reset().
         double seconds() const
         {
-            uint64_t val;
+            uint64_t val = m_base_time;
             QueryPerformanceCounter( (LARGE_INTEGER *)&val );
-            return (val - m_base_time) * freq_;
+            return (val - m_base_time) * m_freq;
         }
         
         /// seconds() returns the number of milliseconds (to very high resolution)
